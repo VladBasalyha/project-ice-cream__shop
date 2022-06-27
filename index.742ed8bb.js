620dbdd6732121cd773ff182402ba4a5cbd2d032
VladBasalyha <100045506+VladBasalyha@users.noreply.github.com>
2022-06-27 16:11:29 +0000

    Deploying to gh-pages from @ VladBasalyha/project-ice-cream__shop@bb6067295c09c9f07141952be56a331302eb3ffd 🚀

--- index.742ed8bb.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".backdrop"),c=document.querySelector(".open-btn"),n=document.querySelector(".close-btn");c.addEventListener("click",(function(){e.classList.remove("is-hidden")})),n.addEventListener("click",(function(){e.classList.add("is-hidden")}));
-//# sourceMappingURL=index.742ed8bb.js.map
